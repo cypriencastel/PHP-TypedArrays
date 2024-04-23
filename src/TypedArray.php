aeda1ea5398af0f2a55e147af0981eab3f966155
Cyprien Castel <cyprien@myqaa.com>
2024-04-23 10:05:19 +0200

    Initial commit

---
+++ src/TypedArray.php
@@ -0,0 +1,43 @@
+<?php
+
+use ArrayAccess;
+
+abstract class TypedArray implements ArrayAccess
+{
+    protected array $items;
+
+    abstract public function satisfies(mixed $item): bool;
+
+    public function __construct(array $items = [])
+    {
+        foreach ($items as $item) {
+            if ($this->satisfies($item)) {
+                $this->items[] = $item;
+            }
+        }
+    }
+
+    public function offsetExists(mixed $offset): bool
+    {
+        return isset($this->items[$offset]);
+    }
+
+    public function offsetGet(mixed $offset): mixed
+    {
+        return isset($this->items[$offset]) ? $this->items[$offset] : null;
+    }
+
+    public function offsetSet(mixed $offset, mixed $value): void
+    {
+        if (is_null($offset)) {
+            $this->items[] = $value;
+        } else {
+            $this->items[$offset] = $value;
+        }
+    }
+
+    public function offsetUnset(mixed $offset): void
+    {
+        unset($this->items[$offset]);
+    }
+}
